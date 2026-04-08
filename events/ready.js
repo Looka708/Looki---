f79a47578a26a4a25e61e51a1e47d6bdf6a8ee49
looka708 <looka.yt123@gmail.com>
2026-04-08 20:05:26 +0500

    feat: implement client ready event with rotating status activities

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,3 @@
-const play = require('play-dl');
-const path = require('path');
-const { parseCookies, cookiesToString } = require('../utils/youtube');
-
 module.exports = {
   name: 'clientReady',
   once: true,
@@ -20,20 +16,5 @@ module.exports = {
       client.user.setActivity(status.name, { type: status.type });
       currentStatus = (currentStatus + 1) % statuses.length;
     }, 30000);
-
-    try {
-        const cookiePath = path.join(__dirname, '../cookies.txt');
-        const cookies = parseCookies(cookiePath);
-        const cookieString = cookiesToString(cookies);
-        
-        if (cookieString) {
-            await play.setToken({ youtube: { cookie: cookieString } });
-            console.log('🌸 [Play-dl] Cookies loaded for YouTube auth');
-        } else {
-            await play.setToken({});
-        }
-    } catch(e) {
-        console.error("Play-dl init optional auth error:", e);
-    }
   },
 };
